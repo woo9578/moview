c5483df2cd23301b1a41831fd831eff7eb396181
woo9578 <top8399@naver.com>
2019-09-03 22:49:35 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit테스트<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; //App.js 를 App으로 손쉽게 사용
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
